c21ccc058f959c38f284330545001837d761fab7
Arpenhayryan <arpenik_hayryan@edu.aua.am>
2018-09-12 16:10:19 +0400

    lol
    lol

---
+++ homework2.js
@@ -0,0 +1,16 @@
+const num1 = 5;
+const num2 = 6;
+const jizn = "bolь";
+console.log (1+2);
+console.log(25/3);
+console.log(233-387);
+console.log(5*90);
+
+if (23 > 63){
+	console.log ('haha');
+	else {
+		console.log('its so hard to come up with these strings when you are trying to impress people')
+	};
+
+};
+//ew
